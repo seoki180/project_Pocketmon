e4f49fbb1b23e3730c44bc6b9ae72e6f1fc0bfea
seoki180 <seoki180@naver.com>
2023-02-03 01:32:02 +0900

    add more picture

--- sever.js
+++ sever.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const host = 3000;
 
-
 app.use(express.static("views"));
 
 app.get("/",function(req,res) {res.sendFile(__dirname + '/index.html')})
 app.get("/receive",function(req,res) {res.sendFile(__dirname + "/views/window.html")})
+
 app.listen(host,function(){
     console.log(`
     ===============
