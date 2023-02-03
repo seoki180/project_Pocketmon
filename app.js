6b81fc238eaa555dd8e02cc863fa1617db838e3e
seoki180 <seoki180@naver.com>
2023-02-04 03:10:36 +0900

    begin to add guest_book

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const fs = require("fs")
 const app = express();
 const host = 3000 || proecess.env.PORT;
+
 
 
 app.use(express.static("views"));
 
 app.get("/",function(req,res) {res.sendFile(__dirname + '/index.html')})
-app.get("/receive",function(req,res) {res.sendFile(__dirname + "/views/window.html")})
+app.get("/take",function(req,res) {res.sendFile(__dirname + "/views/window.html")})
+app.get("/guest_book",function(req,res){
+  console.log(req.query)
+  res.send("hello")
+})
 
 app.listen(host,function(){
     console.log(`
