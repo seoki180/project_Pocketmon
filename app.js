2eccf5a8dfcecf6839808e155ae64f2bb1ac737e
seoki180 <seoki180@naver.com>
2023-02-07 03:05:16 +0900

    fix file to handlebars

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 const express = require("express");
 const handlebars = require("express-handlebars").create({defaultLayout : 'main'})
 const app = express();
-const randPocket = require("./lib/random")
+const getPocketmon = require("./lib/random")
+
+app
+  .use(express.static(__dirname + '/public'))
+  .set('port', process.env.PORT || 3000)
+
+app
+  .engine('handlebars',handlebars.engine)
+  .set('view engine', 'handlebars')
 
 
-app.engine('handlebars',handlebars.engine)
-app.set('view engine', 'handlebars')
-app.set('port', process.env.PORT || 4000)
-app.use(express.static(__dirname + '/public'));
+app
+  .get("/",function(req,res){
+    res.render('home' , {title : "Dr.Oh"})
+  })
+
+  .get('/take',function(req,res){
+    res.render('take',{
+      getPocketmon : getPocketmon.randPocketmon(),
+      title : "your Pocketmon"})
+  })
 
 
-
-
-// app.get("/",function(req,res) {res.sendFile(__dirname + "/public/views/index.html")})
-// app.get("/take",function(req,res) {res.sendFile(__dirname + "/public/views/window.html")})
-// app.get("/guest_book",function(req,res){
-//   console.log(req.query)
-//   res.send("hello")
-// })
-
-app.get("/",function(req,res){
-  res.render('home')
-})
-app.get('/take',function(req,res){
-  res.render('take',{randPocket : randPocket.randPocketmon()})
-})
-
-
-app.listen(app.get('port'), function(){
+  .listen(app.get('port'), function(){
   console.log(`open server at ` + app.get('port'))
-})
+  })
 
 
